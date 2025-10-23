b8c629e9ecff17611442bde051788cdfe219ab48
David <vantory143@gmail.com>
2025-10-23 10:06:50 +0200

    Dodana metoda krog

---
+++ Vaja_6_2/Krog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Krog
+{
+    private int r;
+
+
+    public Krog()
+    {
+        r = 1;
+    }
+    public Krog(int r)
+    {
+        this.r = r;
+    }
+
+    public double Ploscina()
+    {
+        const double pi = 3.14;
+        double obseg = 2 * pi * r;
+        return obseg;
+    }
+
+    public double Obseg()
+    {
+        const double pi = 3.14;
+        return (pi * (r * r));
+    }
+}
+
